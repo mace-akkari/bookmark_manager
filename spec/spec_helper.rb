92347a2186b8cc05a9976ad8ecd569f2d76f4a00
Oli Le Maire <olilemaire85@gmail.com>
2020-10-19 15:39:57 +0100

    passed the second feature test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require 'simplecov-console'
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require 'sinatra'
 
 ENV['RACK_ENV'] = 'test'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
