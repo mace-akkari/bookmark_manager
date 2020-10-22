9800d530a04a31fdcb02b662b80e39b08917bb1b
Mace <maceplus@gmail.com>
2020-10-23 00:50:00 +0100

    Change url

--- spec/features/creating_bookmarks_spec.rb
+++ spec/features/creating_bookmarks_spec.rb
@@ -1,9 +1,10 @@
 feature 'Adding a new bookmark' do
   scenario 'A user can add a bookmark to Bookmark Manager' do
     visit('/bookmarks/new')
-    fill_in('url', with: 'http://testbookmark.com')
+    fill_in('url', with: 'https://sqlzoo.net/wiki/SQL_Tutorial')
+    fill_in('title', with: 'SQL Zoo')
     click_button('Submit')
 
-    expect(page).to have_content 'http://testbookmark.com'
+    expect(page).to have_link('SQL Zoo', href: 'https://sqlzoo.net/wiki/SQL_Tutorial')
   end
 end
