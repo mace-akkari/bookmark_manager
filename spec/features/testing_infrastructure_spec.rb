0a2096d13d2d456f0be97b1964604473f9865685
Mace <maceplus@gmail.com>
2020-10-23 00:49:16 +0100

    Changed content

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,6 @@
 feature 'Testing infrastructure' do
   scenario 'Can run app and check page content' do
     visit('/')
-    expect(page).to have_content 'Hello World!'
+    expect(page).to have_content 'Bookmark Manager'
   end
 end
